4535ef1b779f562bfafee72d7fe86ba5980760be
Joel Beedle <beedlejoel@gmail.com>
2021-12-03 12:14:38 +0000

    Added LibGDX and the relevant architecture generated by LibGDX
    Uses gradle, to open file in IntelliJ select the build.gradle file
    and select 'Open as Project'

---
+++ html/src/com/boatcorp/boatgame/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package com.boatcorp.boatgame.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.boatcorp.boatgame.BoatGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new BoatGame();
+        }
+}
