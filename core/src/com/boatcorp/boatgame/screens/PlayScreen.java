489b94473b40d5987a1b0384378b4afe706d3faa
Joel Beedle <beedlejoel@gmail.com>
2022-01-21 15:18:12 +0000

    Created Point System
    Created class PointSystem and displays current points on screen in PlayScreen. Also adjusted the zoom level. PointSystem utilises a new SpriteBatch mFontBatch to display the font on the screen regardless of what the camera is doing.

--- core/src/com/boatcorp/boatgame/screens/PlayScreen.java
+++ core/src/com/boatcorp/boatgame/screens/PlayScreen.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -12,6 +13,7 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.boatcorp.boatgame.frameworks.PointSystem;
 import com.boatcorp.boatgame.tools.ShapeMaker;
 
 import static com.boatcorp.boatgame.screens.Constants.*;
@@ -19,20 +21,26 @@ import static com.boatcorp.boatgame.screens.Constants.*;
 public class PlayScreen implements Screen {
 
     private final SpriteBatch mBatch;
+    private final SpriteBatch mFontBatch;
     private final World mWorld;
     private final Box2DDebugRenderer mB2dr;
     private final OrthographicCamera mCamera;
     private final Viewport mViewport;
     private final Body mPlayer;
+    private final BitmapFont mFont;
+    private final PointSystem mPoints;
 
     public PlayScreen() {
         mBatch = new SpriteBatch();
+        mFontBatch = new SpriteBatch();
         mWorld = new World(GRAVITY, true);
         mB2dr = new Box2DDebugRenderer();
         mCamera = new OrthographicCamera();
         mCamera.zoom = DEFAULT_ZOOM;
         mViewport = new FitViewport(640 / PPM, 480 / PPM, mCamera);
         mPlayer = ShapeMaker.createRectangle(new Vector2(0,0), new Vector2(64, 128), BodyDef.BodyType.DynamicBody, mWorld, 0.4f);
+        mFont = new BitmapFont(Gdx.files.internal("fonts/korg.fnt"), Gdx.files.internal("fonts/korg.png"), false);
+        mPoints = new PointSystem();
     }
 
     @Override
@@ -50,8 +58,18 @@ public class PlayScreen implements Screen {
     }
 
     private void draw() {
+        // mFontBatch drawing
+        mFontBatch.begin();
+        mFont.getData().setScale(0.5f);
+        String displayPoint = "SCORE:" + mPoints.getPoints();
+        mFont.draw(mFontBatch, displayPoint, 8, 472);
+
+        mFontBatch.end();
+
+        // mBatch drawing
         mBatch.setProjectionMatrix(mCamera.combined);
         mB2dr.render(mWorld, mCamera.combined);
+
     }
 
     private void update(final float delta) {
@@ -83,6 +101,7 @@ public class PlayScreen implements Screen {
     @Override
     public void dispose() {
         mBatch.dispose();
+        mFontBatch.dispose();
         mWorld.dispose();
         mB2dr.dispose();
     }
