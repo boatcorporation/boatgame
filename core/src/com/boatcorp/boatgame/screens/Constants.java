cceb410691874b6b458befa88805fd01b5c226f5
wtljohnson <johnsonw167@gmail.com>
2022-01-21 16:54:29 +0000

    Fixed player position with the new scale

--- core/src/com/boatcorp/boatgame/screens/Constants.java
+++ core/src/com/boatcorp/boatgame/screens/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
 
     public static final Vector2 GRAVITY = new Vector2(0,0);
     public static final float DEFAULT_ZOOM = 3f;
-    public static final float PPM = 50.0f;
+    public static final float PPM = 25.0f;
 
     public static final String MAP_NAME = "Maps/basic_map.tmx";
     public static final String PLAYER = "Player";
--- core/src/com/boatcorp/boatgame/screens/PlayScreen.java
+++ core/src/com/boatcorp/boatgame/screens/PlayScreen.java
@@ -35,7 +35,7 @@ public class PlayScreen implements Screen {
         mCamera.zoom = DEFAULT_ZOOM;
         mViewport = new FitViewport(640 / PPM, 480 / PPM, mCamera);
         mMapLoader = new MapLoader(mWorld);
-        mPlayer = ShapeMaker.createRectangle(new Vector2(2500,2500), new Vector2(64,128), BodyDef.BodyType.DynamicBody, mWorld, 0.4f);
+        mPlayer = ShapeMaker.createRectangle(new Vector2(1250, 1250), new Vector2(64,128), BodyDef.BodyType.DynamicBody, mWorld, 0.4f);
 
     }
 
